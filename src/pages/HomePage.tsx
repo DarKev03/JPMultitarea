6a6d864154c056091ebb0e3cacdd8106af58b45d
Victor Tercero <victor.tercerom@viewnext.com>
2025-05-06 16:36:56 +0200

    Footer Creado. TODO: Cambiar las URL de los iconos de insta y FB

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 // src/pages/HomePage.tsx
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const HomePage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 scroll-smooth">
       <Navbar />
+      <main className="space-y-32 px-8 py-16">
+        {/* ... secciones ... */}
+        {/* Mantengo este spacer para que se diferencie el footer del header, eliminar cuando hagomos el body*/}
+      </main>
+      <Footer />
     </div>
   );
 };
