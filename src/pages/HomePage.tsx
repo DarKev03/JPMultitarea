4f81ccf4b3c21567958b012767a58e7573bea427
DarKev03 <kevingarciah03@gmail.com>
2025-05-07 12:42:19 +0200

    Refactor HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,15 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import GaleriaReformas from '../components/GaleriaReformas';
 import HeroImage from '../components/HeroImage';
 import ProductCarousel from '../components/ProductCarousel';
+import GaleriaReformas from '../components/GaleriaReformas';
 
 const HomePage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 scroll-smooth">
       <Navbar />
-
-      <main className="space-y-32 py-16">
-        <main className="flex-1 ">        
+      <main className="flex-1 ">        
         <HeroImage />
         <ProductCarousel />
         <GaleriaReformas />
