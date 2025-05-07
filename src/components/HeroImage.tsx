0409f2883bf022ac652edaf97ec6dda7863bee70
DarKev03 <kevingarciah03@gmail.com>
2025-05-07 13:04:01 +0200

    cambios a mejor

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -4,9 +4,9 @@ const HeroImage: React.FC = () => {
     return (
         <section className="w-full shadow-md">
             <img
-                src={'src/assets/Mi imagen de ChatGPT.png'}
+                src={'src/assets/HeroImage.png'}
                 alt="Banner Reformas Integrales"
-                className="w-full h-100 object-contain cursor-pointer transition duration-300 hover:brightness-95"
+                className="w-full h-120 object-cover cursor-pointer transition duration-300 hover:brightness-95"
             />
         </section>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
 
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
-      <div className="max-w-4xl mx-auto px-4 py-4 flex flex-col md:flex-row justify-between items-center">
+      <div className="max-w-4xl mx-auto px-4 py-8 flex flex-col md:flex-row justify-between items-center">
         {/* Logo and Menu Toggle */}
         <div className="w-full flex justify-between items-center">
           <img src={'src/assets/Logo.png'} alt="JP Logo" className="h-12 w-auto object-contain min-w-[3rem]" />
@@ -19,10 +19,10 @@ const Navbar: React.FC = () => {
         <nav className={`${
           isOpen ? 'flex' : 'hidden'
         } md:flex flex-col md:flex-row items-center justify-center md:space-x-2 gap-4 mt-4 md:mt-0`}>
-          <a href="#servicios" className="whitespace-nowrap bg-white px-6 py-2 rounded-full shadow-md hover:shadow-lg transition duration-200">Servicios</a>
-          <a href="#especialidades" className="whitespace-nowrap bg-white px-6 py-2 rounded-full shadow-md hover:shadow-lg transition duration-200">Especialidades</a>
-          <a href="#sobre-nosotros" className="whitespace-nowrap bg-white px-6 py-2 rounded-full shadow-md hover:shadow-lg transition duration-200">Sobre nosotros</a>
-          <a href="#contacto" className="whitespace-nowrap bg-white px-6 py-2 rounded-full shadow-md hover:shadow-lg transition duration-200">Contacto</a>
+          <a href="#servicios" className="whitespace-nowrap bg-white px-6 py-2 rounded-full  hover:shadow-lg transition duration-200">Servicios</a>
+          <a href="#especialidades" className="whitespace-nowrap bg-white px-6 py-2 rounded-full  hover:shadow-lg transition duration-200">Especialidades</a>
+          <a href="#sobre-nosotros" className="whitespace-nowrap bg-white px-6 py-2 rounded-full  hover:shadow-lg transition duration-200">Sobre nosotros</a>
+          <a href="#contacto" className="whitespace-nowrap bg-white px-6 py-2 rounded-full  hover:shadow-lg transition duration-200">Contacto</a>
         </nav>
       </div>
     </header>
